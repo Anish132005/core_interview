37eada33a7c9d169eb7ac016253aa08dea79f41c
Anish132005 <malviyaanish25@gmail.com>
2025-09-09 16:06:12 +0530

    6th

---
+++ src/com/rays/string/ReverseString.java
@@ -0,0 +1,21 @@
+package com.rays.string;
+
+import java.util.Iterator;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String name = "Anish Malviya";
+		
+		for (int i = name.length() -1; i >=0; i--) {
+			
+			System.out.print(name.charAt(i));
+			
+		}		
+		
+		
+
+	}
+
+}
