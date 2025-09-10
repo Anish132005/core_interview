92531db13ff79c09cc08f78d55f94ab4b1e43bb1
Anish132005 <malviyaanish25@gmail.com>
2025-09-10 17:27:24 +0530

    12th

---
+++ src/com/rays/string/StringBufferMethod.java
@@ -0,0 +1,17 @@
+package com.rays.string;
+
+public class StringBufferMethod {
+
+	public static void main(String[] args) {
+		
+		
+		StringBuffer sb =  new StringBuffer("Anish");
+		
+		sb.append("Malviya");
+		
+		System.out.println(sb);
+		
+
+	}
+
+}
