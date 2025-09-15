166d15bacffa82442233b893b01056f6280c0a80
Anish132005 <malviyaanish25@gmail.com>
2025-09-15 13:58:09 +0530

    14th

--- src/com/rays/string/Concat.java
+++ src/com/rays/string/Concat.java
@@ -5,7 +5,7 @@ public class Concat {
 	public static void main(String[] args) {
 		
 		String a = "Rays";
-		String b = "technology";
+		String b = "Technology";
 		
 		System.out.println(a.concat(b));
 
